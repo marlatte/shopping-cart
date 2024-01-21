2a46333ccb04d0818653932d1c09a14cab06bab6
MarLatte <86508134+marlatte@users.noreply.github.com>
2024-01-21 17:12:15 -0500

    Create product cards

---
+++ src/components/product-displays/ProductCard.jsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default function ProductCard({ data }) {
+  return (
+    <Link to="/">
+      <div className="img-container">
+        <img src={data.image} alt={data.title} />
+      </div>
+      <div className="card-body">
+        <h4 className="card-title truncate" title={data.title}>
+          {data.title}
+        </h4>
+        <p
+          className="price"
+          aria-label={`Price: $${data.price}`}
+        >{`$${data.price}`}</p>
+      </div>
+    </Link>
+  );
+}
+
+ProductCard.propTypes = {
+  data: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    rating: PropTypes.shape({
+      rate: PropTypes.number.isRequired,
+      count: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
---
+++ src/components/product-displays/__tests__/ProductCard.test.jsx
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react';
+import { expect, test } from 'vitest';
+import { BrowserRouter } from 'react-router-dom';
+import ProductCard from '../ProductCard';
+
+const testProduct = {
+  id: 1,
+  title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+  price: 109.95,
+  description:
+    'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+  category: "men's clothing",
+  image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+  rating: { rate: 3.9, count: 120 },
+};
+
+function setup(data) {
+  return render(<ProductCard data={data} />, { wrapper: BrowserRouter });
+}
+
+test('has 2 children: image & body', () => {
+  setup(testProduct);
+  expect(screen.getByRole('link').childElementCount).toBe(2);
+});
+
+test('displays an image with a src and alt text', () => {
+  setup(testProduct);
+  expect(screen.getByRole('img').src).toBe(testProduct.image);
+  expect(screen.getByRole('img')).toHaveAccessibleName(testProduct.title);
+});
+
+test('displays a product name', () => {
+  setup(testProduct);
+  expect(screen.getByRole('heading').textContent).toMatch(testProduct.title);
+});
+
+test('displays a price', () => {
+  const { container } = setup(testProduct);
+  const price = container.querySelector('.price');
+  expect(price).toHaveAccessibleName(`Price: $${testProduct.price}`);
+  expect(price.textContent).toBe(`$${testProduct.price}`);
+});
