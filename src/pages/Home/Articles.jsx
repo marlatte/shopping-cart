693373eabd5fca20e668f68ff381fa010f6dcfaf
MarLatte <86508134+marlatte@users.noreply.github.com>
2024-01-23 14:41:10 -0500

    Finish articles section

--- src/pages/Home/Articles.jsx
+++ src/pages/Home/Articles.jsx
@@ -1,1 +1,39 @@
-export default function Articles() {}
+import PropTypes from 'prop-types';
+import shirtsUrl from '../../assets/shirts-on-hanger.jpg';
+import modelUrl from '../../assets/model-in-field.jpg';
+
+export default function Articles() {
+  return (
+    <section className="articles">
+      <Entry
+        imgUrl={shirtsUrl}
+        imgAlt="Shirts on a hanger"
+        title="What are the best ways to keep your clothing looking new?"
+      />
+      <Entry
+        imgUrl={modelUrl}
+        imgAlt="A stylish model in a field"
+        title="Wondering how to dress stylishly? We've got you."
+      />
+    </section>
+  );
+}
+
+function Entry({ imgUrl, imgAlt, title }) {
+  return (
+    <div className="entry">
+      <div className="fake-link">
+        <img src={imgUrl} alt={imgAlt} />
+      </div>
+      <div className="fake-link">
+        <h2>{title}</h2>
+      </div>
+    </div>
+  );
+}
+
+Entry.propTypes = {
+  imgUrl: PropTypes.string.isRequired,
+  imgAlt: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
--- src/pages/Home/__tests__/Articles.test.jsx
+++ src/pages/Home/__tests__/Articles.test.jsx
@@ -7,6 +7,20 @@ function setup() {
   return render(<Articles />, { wrapper: BrowserRouter });
 }
 
-test.todo('', () => {
-  expect();
+test('has 2 entries, each with an image and a heading', () => {
+  setup();
+
+  expect(screen.getAllByRole('img')).toHaveLength(2);
+  expect(screen.getAllByRole('heading')).toHaveLength(2);
+});
+
+test('image urls match', () => {
+  setup();
+
+  expect(screen.getByRole('img', { name: /shirts on a hanger/i }).src).toMatch(
+    /shirts-on-hanger.jpg/i
+  );
+  expect(screen.getByRole('img', { name: /stylish model/i }).src).toMatch(
+    /model-in-field.jpg/i
+  );
 });
