08d7a351dbc2edbc437465566b361a47a84eddb0
MarLatte <86508134+marlatte@users.noreply.github.com>
2024-01-23 21:20:36 -0500

    Write Homepage useEffect sequence

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,31 @@
+import { useEffect, useState } from 'react';
 import Hero from './Hero';
 import CallToAction from './CallToAction';
 import Articles from './Articles';
 import ProductTeaser from './ProductTeaser';
+import { getRandom3, fetchItems } from '../../utils/fetch-data';
 
 export default function Home() {
-  // Get jewelry products, random sort, pick first 3
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    let ignore = false;
+    async function getJewelry() {
+      const jewelry = await fetchItems({ category: 'jewelery' });
+      if (!ignore) setProducts(getRandom3(jewelry));
+    }
+
+    getJewelry();
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
   return (
     <main className="home">
       <Hero />
-      <ProductTeaser />
+      <ProductTeaser products={products} />
       <Articles />
       <CallToAction />
     </main>
