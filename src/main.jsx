31150726635dbe8e8618c94ff1168cbeeabbecca
MarLatte <86508134+marlatte@users.noreply.github.com>
2024-01-27 14:49:39 -0500

    Update name from App to Root

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import App from './App';
+import Root from './Root';
 import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <Root />,
     // errorElement: <ErrorPage />,
   },
   {
